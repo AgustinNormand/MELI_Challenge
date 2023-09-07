0f6a9fb9c655bfa41cb954f352f50f0ebea29472
Agustin <48933518+AgustinNormand@users.noreply.github.com>
2023-09-07 12:31:34 -0300

    Acomodo la lógica de la API, método de clientes consumidores y un primer test sobre esto

--- Desafío_1/criticity_api.py
+++ Desafío_1/criticity_api.py
@@ -1,14 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, Header
+from fastapi import FastAPI, HTTPException, Header, APIRouter
 from pydantic import BaseModel
 from datetime import datetime
 from itertools import count
-
-app = FastAPI()
-
-app_states = {}
-app_last_request_time = {}
-processed_id_counter = count(0)
 
 class StateChange(BaseModel):
     current_status: int
@@ -16,32 +10,53 @@ class StateChange(BaseModel):
 
 class Criticity_API():
     def __init__(self):
-        self.process_id = 0
+        self.processed_id_counter = count(0)
+        self.clients_apps_requests_timestamps = {}
+        self.last_criticity_update_value = None
+        self.last_criticity_update_timestamp = None
+        self.last_criticity_update_processed_id = None
 
-    @app.get("/secapp/update", response_model=dict)
-    async def get_last_state(App_name: str = Header(None)):
+        self.router = APIRouter()
+        self.router.add_api_route("/secapp/update", self.get_last_state, methods=["GET"])
+
+    async def get_last_state(self, App_name: str = Header(None)):
         if not App_name:
             raise HTTPException(status_code=400, detail="Falta el encabezado App-name")
 
-        if App_name in app_states:
-            return app_states[App_name]
+        if self.last_criticity_update_value is None or \
+                self.clients_apps_requests_timestamps[App_name] >= self.last_criticity_update_timestamp:
+            return {}
+
+        if App_name in self.clients_apps_requests_timestamps.keys():
+            if self.clients_apps_requests_timestamps[App_name] < self.last_criticity_update_timestamp:
+                return {"current_status": self.last_criticity_update_value,
+                        "status_change_id": self.last_criticity_update_processed_id,
+                        "Change_at": self.last_criticity_update_timestamp}
         else:
-            raise HTTPException(status_code=404, detail="No se encontró ningún estado para la aplicación")
+            self.clients_apps_requests_timestamps[App_name] = self.last_criticity_update_timestamp
 
-    @app.post("/secapp/update", response_model=dict)
-    async def update_state(state: StateChange, App_name: str = Header(None)):
-        if not App_name:
-            raise HTTPException(status_code=400, detail="Falta el encabezado App-name")
+            return {"current_status": self.last_criticity_update_value,
+                    "status_change_id": self.last_criticity_update_processed_id,
+                    "Change_at": self.last_criticity_update_timestamp}
 
-        # Obtener el siguiente processed_id autoincremental
-        processed_id = next(processed_id_counter)
+    # @app.post("/secapp/update", response_model=dict)
+    # async def update_state(self, state: StateChange, App_name: str = Header(None)):
+    #    if not App_name:
+    #        raise HTTPException(status_code=400, detail="Falta el encabezado App-name")
 
-        # Almacenar el último cambio de estado con el processed_id autoincremental
-        app_states[App_name] = {"current_status": state.dict()["current_status"], "status_change_id": processed_id, "Change_at": state.dict()["Change_at"]}
+    # Obtener el siguiente processed_id autoincremental
 
-        # Registrar el timestamp de la solicitud
-        app_last_request_time[App_name] = datetime.now()
-        return {"processed_id": processed_id}
+    # processed_id = next(self.processed_id_counter)
+
+    # Almacenar el último cambio de estado con el processed_id autoincremental
+    # app_states[App_name] = {"current_status": state.dict()["current_status"], "status_change_id": processed_id, "Change_at": state.dict()["Change_at"]}
+
+    # last_criticity_update_timestamp = state.dict()["Change_at"]
+
+    # return {"processed_id": processed_id}
 
 if __name__ == "__main__":
+    app = FastAPI()
+    criticaly_api = Criticity_API()
+    app.include_router(criticaly_api.router)
     uvicorn.run(app, host="0.0.0.0", port=8000)
