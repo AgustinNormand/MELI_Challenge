0163cf62a3eec36c2b47aad8a741d5bccfbaddba
Agustin <48933518+AgustinNormand@users.noreply.github.com>
2023-09-09 13:38:12 -0300

    Primera versión que exporta datos necesarios para responder la primera pregunta

--- Desafío_2/app/downtime_file_analyzer.py
+++ Desafío_2/app/downtime_file_analyzer.py
@@ -1,14 +1,15 @@
 from datetime import datetime
-import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+import pandas as pd
+import calendar
 
 FILENAME = "Datos_uptime_challenge.tsv"
-
 class DowntimeFileAnalyzer:
     def __init__(self):
-        self.timestamp_lines = {}
-        self.other_lines = {}
+        self.timestamp_lines = []
+        self.other_lines = []
+
+        # Variable utilizada para estimar la fecha de ocurrencia de las lineas sin timestamp
+        self.latest_start_timestamp = None
 
     # Función personalizada para analizar fechas y horas de diferentes formatos
     def parse_date(self, date_str):
@@ -26,24 +27,34 @@ class DowntimeFileAnalyzer:
         print(f"Error: Formato de fecha desconocido: {date_str}")
         return date_str
 
-    def sort_timestamps(self, inicio, fin):
-        if inicio > fin:
-            return fin, inicio
+    def sort_timestamps(self, timestamp_1, timestamp_2):
+        if timestamp_1 > timestamp_2:
+            return timestamp_2, timestamp_1
         else:
-            return inicio, fin
+            return timestamp_1, timestamp_2
 
     def add_timestamp_line(self, app_name, inicio, fin):
-        duration = (fin - inicio).total_seconds() / 60
-        if app_name in self.timestamp_lines:
-            self.timestamp_lines[app_name].append([inicio, fin, duration])
+        if inicio.year != fin.year or inicio.month != fin.month or inicio.day != fin.day:
+            print(f"Warning: Timestamps from different years, months or days: {inicio} - {fin}")
+            current_date = inicio
+            while current_date < fin:
+                if (fin - current_date) < pd.Timedelta(days=1):
+                    incremented_date = fin
+                else:
+                    incremented_date = current_date + pd.Timedelta(days=1)
+
+                duration = (incremented_date - current_date).total_seconds() / 60
+                self.timestamp_lines.append([current_date, incremented_date, duration, app_name])
+                self.latest_start_timestamp = current_date
+
+                current_date = incremented_date
         else:
-            self.timestamp_lines[app_name] = [[inicio, fin, duration]]
+            duration = (fin - inicio).total_seconds() / 60
+            self.timestamp_lines.append([inicio, fin, duration, app_name])
+            self.latest_start_timestamp = inicio
 
     def add_other_line(self, app_name, message):
-        if app_name in self.other_lines:
-            self.other_lines[app_name].append(message)
-        else:
-            self.other_lines[app_name] = [message]
+        self.other_lines.append([app_name, self.latest_start_timestamp, message])
 
     def process_line(self, line):
         columns = line.strip().split('\t')
@@ -69,3 +80,47 @@ class DowntimeFileAnalyzer:
                     self.process_line(line)
                 except Exception as e:
                     print(f"Error {e} prosessing line {line}")
+
+        self.create_dataframe()
+
+    def create_dataframe(self):
+        data = pd.DataFrame(self.timestamp_lines, columns=['inicio', 'fin', 'duration', 'app_name'])
+        print(f"Number of duplicated rows {data.duplicated().sum()}")
+        self.dataframe = data.drop_duplicates()
+
+    def get_uptime(self, dataframe, year):
+        if dataframe.empty:
+            return 100
+        else:
+            days = 366 if calendar.isleap(year) else 365
+            duracion_total = days * 24 * 60 * 60
+            duracion_downtime = (dataframe["fin"] - dataframe["inicio"]).sum().total_seconds()
+            return ((duracion_total - duracion_downtime) / duracion_total) * 100
+
+    def get_available_years(self):
+        return list(range(self.dataframe["inicio"].min().year, self.dataframe["fin"].max().year + 1))
+
+    def get_available_apps(self):
+        return self.dataframe["app_name"].unique()
+
+    def select_data(self, year, app_name):
+        selected_data = self.dataframe[
+            ((self.dataframe["inicio"].dt.year == year) | (self.dataframe["fin"].dt.year == year)) & (
+                        self.dataframe["app_name"] == app_name)]
+
+        return selected_data
+
+    def export_year_csv(self):
+        with open("year_downtimes.csv", 'w') as f:
+            f.write("app_name,year,uptime\n")
+            for year in self.get_available_years():
+                for app_name in self.get_available_apps():
+                    seleced_data = self.select_data(year, app_name)
+                    f.write(f"{app_name},{year},{self.get_uptime(seleced_data, year)}\n")
+
+    def export_cleaned_csv(self):
+        self.dataframe.to_csv("downtimes.csv", index=False)
+
+    def export_results(self):
+        self.export_year_csv()
+        return
--- Desafío_2/app/main.py
+++ Desafío_2/app/main.py
@@ -3,4 +3,5 @@ from app.downtime_file_analyzer import DowntimeFileAnalyzer
 if __name__ == "__main__":
     dfa = DowntimeFileAnalyzer()
     dfa.analyze_file()
+    dfa.export_results()
 
