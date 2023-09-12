fc936b1b7fbc194244f3fd9e5b29cd58b35ec7e3
Agustin <48933518+AgustinNormand@users.noreply.github.com>
2023-09-12 17:29:44 -0300

    Agrego la persistencia en influx en una petición cliente

--- Desafío_1/app/Secapp.py
+++ Desafío_1/app/Secapp.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, HTTPException, Header, APIRouter
+from fastapi import HTTPException, Header, APIRouter
 from pydantic import BaseModel
 from datetime import datetime
 from itertools import count
 import os
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+import time
 
 class StateChange(BaseModel):
     current_status: int
@@ -11,34 +14,56 @@ class StateChange(BaseModel):
 
 class Secapp():
     def __init__(self):
-        self.processed_id_counter = count(0)
-        self.clients_apps_last_update_timestamps = {}
+        self.processed_id_counter = count(1)
+        self.clients_apps_last_update_timestamps = {}  # DATABASE MISSING
 
-        self.last_criticity_update_value = None
-        self.last_criticity_update_timestamp = None
-        self.last_criticity_update_processed_id = None
+        client = InfluxDBClient(url=self.get_config("INFLUXDB_URL"),
+                                token=self.get_config("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"),
+                                org=self.get_config("INFLUXDB_ORG"), debug=True)
 
-        self.clients_apps_requests_timestamps = {}
+        self.write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        self.last_criticity_update_value = None  # DATABASE MISSING
+        self.last_criticity_update_timestamp = None  # DATABASE MISSING
+        self.last_criticity_update_processed_id = None  # DATABASE MISSING
+
+        # self.clients_apps_requests_timestamps = {} # ?
 
         self.router = APIRouter()
         self.router.add_api_route("/secapp/update", self.get_last_state, methods=["GET"])
         self.router.add_api_route("/secapp/update", self.update_state, methods=["POST"])
 
+    def get_config(self, key):
+        return os.getenv(key)
+
+    def register_request(self, App_name, timestamp):
+        point = {
+            "measurement": "requests",
+            "tags": {
+                "app_name": App_name
+            },
+            "time": timestamp,
+            "fields": {
+                "cantidad": 1
+            }
+        }
+        self.write_api.write(bucket=self.get_config("INFLUXDB_BUCKET_CONSUMERS"), record=point, time=timestamp, write_precision='s')
+
     async def get_last_state(self,
                              App_name: str = Header(None),
                              token: str = Header(None)):
 
-        if token != os.getenv("APIKEY"):
+        if token != self.get_config("APIKEY"):
             raise HTTPException(
                 status_code=401,
-                detail="wrong api key",
+                detail="Valor incorrecto del API Token",
                 headers={"WWW-Authenticate": "Bearer"},
             )
 
         if not App_name:
             raise HTTPException(status_code=400, detail="Falta el encabezado App-name")
 
-        self.clients_apps_requests_timestamps[App_name] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        self.register_request(App_name, int(time.time()))
 
         if self.last_criticity_update_value is None:
             return {}
@@ -60,12 +85,12 @@ class Secapp():
     async def update_state(self,
                            state: StateChange,
                            App_name: str = Header(None),
-                            token: str = Header(None)):
+                           token: str = Header(None)):
 
         if token != os.getenv("APIKEY"):
             raise HTTPException(
                 status_code=401,
-                detail="wrong api key",
+                detail="Valor incorrecto del header Token",
                 headers={"WWW-Authenticate": "Bearer"},
             )
 
