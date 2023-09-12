8c2793df8652ec5ef9a7f441a627a8387c313138
Agustin <48933518+AgustinNormand@users.noreply.github.com>
2023-09-12 18:38:31 -0300

    Agrego el producer

---
+++ Desafío_1/producer/app/main.py
@@ -0,0 +1,5 @@
+from producer import Producer
+
+if __name__ == '__main__':
+    producer = Producer()
+    producer.run()
--- Desafío_1/producer/app/producer.py
+++ Desafío_1/producer/app/producer.py
@@ -3,18 +3,18 @@ import random
 import time
 import requests
 import logging
+from datetime import datetime
 
-
-class Consumer():
+class Producer():
     def __init__(self):
         logging.basicConfig(level=logging.DEBUG)
-        self.logger = logging.getLogger('ConsumerLogger')
+        self.logger = logging.getLogger('ProducerLogger')
         stream_handler = logging.StreamHandler()
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         stream_handler.setFormatter(formatter)
         self.logger.addHandler(stream_handler)
 
-        self.logger.info('Starting Consumer')
+        self.logger.info('Starting Producer')
 
         self.headers = {
             'App-name': self.get_config('APPNAME'),
@@ -26,8 +26,17 @@ class Consumer():
 
     def run(self):
         while True:
-            response = requests.get(self.get_config("API_URL"), headers=self.headers)
+            # Genero un valor aleatorio entre 1 y 10
+            random_value = random.randrange(1, 10 + 1)
+
+            data = {
+                'current_status': random_value,
+                'Change_at': datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+            }
+
+            response = requests.post(url=self.get_config("API_URL"), headers=self.headers, json=data)
             self.logger.info(f"Response text: {response.text}")
+
             # Genera un valor aleatorio entre 3 y 7 minutos en segundos
             seconds_random_time = random.randrange(3 * 60, 7 * 60 + 1)
             time.sleep(seconds_random_time)
