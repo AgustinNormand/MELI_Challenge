e508f1ed842eccd60d00fb4eed96c777a1f4b453
Agustin <48933518+AgustinNormand@users.noreply.github.com>
2023-09-12 18:24:26 -0300

    Arreglo bug en el consumer

--- Desafío_1/consumer/app/consumer.py
+++ Desafío_1/consumer/app/consumer.py
@@ -2,11 +2,22 @@ import os
 import random
 import time
 import requests
+import logging
+
 
 class Consumer():
     def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+        self.logger = logging.getLogger('DowntimeFileAnalyzerLogger')
+        stream_handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
+
+        self.logger.info('Starting Consumer')
+
         self.headers = {
-            'App_name': self.get_config('APPNAME'),
+            'App-name': self.get_config('APPNAME'),
             'token': self.get_config('APIKEY')
         }
 
@@ -15,7 +26,8 @@ class Consumer():
 
     def run(self):
         while True:
-            requests.get(self.get_config("API_URL"), headers=self.headers)
+            response = requests.get(self.get_config("API_URL"), headers=self.headers)
+            self.logger.info(f"Response text: {response.text}")
             # Genera un valor aleatorio entre 3 y 7 minutos en segundos
             seconds_random_time = random.randrange(3 * 60, 7 * 60 + 1)
             time.sleep(seconds_random_time)
